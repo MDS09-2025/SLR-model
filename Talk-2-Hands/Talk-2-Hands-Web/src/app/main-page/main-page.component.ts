6577e307a96d3de0be497aefb72882b1ddf77758
xuen-han <sheexuenhan@gmail.com>
2025-08-10 18:45:46 +0800

    Organise them into Talk-2-Hands-Web folder

---
+++ Talk-2-Hands/Talk-2-Hands-Web/src/app/main-page/main-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { TranslateService } from '../services/translate.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-main-page',
+  imports: [NavBarComponent, FormsModule],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+  mediaLink: string = '';
+
+  constructor(private translateService: TranslateService) {}
+
+  sendLink() {
+    this.translateService.sendLink(this.mediaLink).subscribe({
+      next: (res) => console.log('Backend response:', res),
+      error: (err) => console.error('Error sending link:', err)
+    });
+  }
+}
